73e1916d5ba6aec2b4273fd872211658a04acb0b
BlackNord <technic_pr@mail.ru>
2022-05-23 14:42:17 +0300

    photo-keeper-api-images - models were added

---
+++ PhotoKeeper.Api/PhotoKeeper.Api/Models/AccountDtos/Responses/PhotoResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace PhotoKeeper.Api.Models.AccountDtos.Responses;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class PhotoResponse
+{
+	[Key]
+	public int Id { get; set; }
+
+	public string? PhotoName { get; set; }
+
+	public string? Description { get; set; }
+
+	public string ImageName { get; set; }
+
+	[NotMapped]
+	public IFormFile? ImageFile { get; set; }
+
+	[NotMapped]
+	public string? ImageSrc { get; set; }
+
+	public int UserId { get; set; }
+}
